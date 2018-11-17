4fbf752e587384b406a889c2c1e6a21a5f9f41c8
wjpjw <recvfromsockaddr@gmail.com>
2018-11-17 14:25:41 +0800

    w

---
+++ libasync/build/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wjp/Desktop/workspace/libasync/libasync/tests
+# Build directory: /Users/wjp/Desktop/workspace/libasync/libasync/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
