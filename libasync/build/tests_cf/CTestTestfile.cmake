5d91908484860fecc7480042d0bea668c741cd7b
root <root@DESKTOP-07C9913.localdomain>
2019-01-06 23:29:49 +0800

    w

--- libasync/build/tests_cf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/workspace/libasync/libasync/tests_cf
-# Build directory: /mnt/d/workspace/libasync/libasync/build/tests_cf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
